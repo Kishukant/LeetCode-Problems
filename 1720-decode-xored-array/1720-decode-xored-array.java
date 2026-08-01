04eb0dc2a17f2666666cc7cf5db815f6b64ffc43
Kishukant <kishukant792@gmail.com>
2026-08-01 20:17:16 +0530

    Time: 3 ms (0.75%), Space: 48 MB (26.44%) - LeetHub

--- 1720-decode-xored-array/1720-decode-xored-array.java
+++ 1720-decode-xored-array/1720-decode-xored-array.java
@@ -2,8 +2,8 @@ class Solution {
     public int[] decode(int[] encoded, int first) {
         int n=encoded.length;
         int[] arr= new int[n+1];
+        arr[0]=first;
         for(int i=1;i<=n;i++){
-            arr[0]=first;
             arr[i]=Math.abs(encoded[i-1]^arr[i-1]);
         }
         return arr;
