6c210330d8d708d1262814a03c1d4318293da9f9
Kishukant <kishukant792@gmail.com>
2026-08-03 08:33:56 +0530

    Time: 0 ms (100%), Space: 19.4 MB (17.67%) - LeetHub

---
+++ 3065-minimum-operations-to-exceed-threshold-value-i/3065-minimum-operations-to-exceed-threshold-value-i.py
@@ -0,0 +1,8 @@
+class Solution:
+    def minOperations(self, nums: List[int], k: int) -> int:
+        count=0
+        for n in nums:
+            if n<k:
+                count=count+1
+        return count    
+        
